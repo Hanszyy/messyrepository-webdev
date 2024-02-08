1b6feadb8d16721a8c1ea708f0783ef6502edf64
hanszyy <hanswong117@gmail.com>
2024-02-08 19:28:03 +0800

    form submission with html

--- backend-learning3/app.js
+++ backend-learning3/app.js
@@ -1,36 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
 
-// Middleware for parsing JSON and URL-encoded form data
+// Middleware for parsing JSON and URL-encoded
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Define a route for serving an HTML form
+// Serve static files like CSS, images
+app.use(express.static('public'));
+
+// A route for serving the HTML form
 app.get('/', (req, res) => {
-    res.send(`
-        <form action="/submit-form" method="post">
-            <input type="text" name="username" placeholder="Enter your username" required>
-            <input type="password" name="password" placeholder="Enter your super secret favourite word" required>
-            <button type="submit what you've written on here bro">Submit</button>
-        </form>
-    `);
+    res.sendFile(__dirname + '/index.html');
 });
 
-// route to handle form submissions
+// Route to handle form submissions
 app.post('/submit-form', (req, res) => {
     // Access submitted form data
     const { username, password } = req.body;
 
-    // Here is where it would typically validate the data and save it to a database
+    // Validate data
+    if (!username) {
+        return res.status(400).send('Pretty please enter your username');
+    }
 
+    // *Note*  here is typically where it validates the data further and save it to a database
+    // such as username and password for this instance
     // Send a response back to the client
-    res.send(`Form submitted successfully! Username: ${username} \n Password: You should've remember it yourself`);
+    res.send(`Form submitted successfully! Username: ${username}`);
 });
 
 // Start server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
